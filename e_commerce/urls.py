e4901d7395204dceb4ad631ed0100df7120b1e97
paatrickreis <patrick@setrix.lan>
2019-12-18 15:55:55 -0300

    aula ate a 5

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page),
+    path('contato/', contact_page),
+    path('sobre/', about_page),
+    path('login/',login_page),
+    path('registro/', register_page),
+
 ]
